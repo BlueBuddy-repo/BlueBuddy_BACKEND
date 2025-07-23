f0244cc166082469f944129b2ee71ad9fa4d399a
Minseon Jeon <mminnn28@gmail.com>
2025-07-23 20:13:48 +0900

    [Build] 기초 세팅

---
+++ src/main/java/com/soohwang/bluebuddy/BlueBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.soohwang.bluebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soohwang/bluebuddy/BlueBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soohwang.bluebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
