e3ff18cba28969606cfbed20f1d8caec53baf3c4
yyen <brenda1217@naver.com>
2025-07-31 18:43:34 +0900

    #3[feat]:보유 생물 조회

---
+++ src/main/java/com/soohwang/bluebuddy/dto/UserCreatureDto.java
@@ -0,0 +1,17 @@
+package com.soohwang.bluebuddy.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserCreatureDto {
+    private Long creatureId;
+    private String nameKr;
+    private String nameEn;
+    private String scientificName;
+    private String habitat;
+    private Long endangermentLevel;
+    private String description;
+}
--- src/main/java/com/soohwang/bluebuddy/repository/UserCreatureRepository.java
+++ src/main/java/com/soohwang/bluebuddy/repository/UserCreatureRepository.java
@@ -4,7 +4,14 @@ import com.soohwang.bluebuddy.entity.SeaCreature;
 import com.soohwang.bluebuddy.entity.User;
 import com.soohwang.bluebuddy.entity.UserCreature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface UserCreatureRepository extends JpaRepository<UserCreature, Long> {
     boolean existsByUserAndSeaCreature(User user, SeaCreature creature);
+
+    @Query("select uc.seaCreature from UserCreature uc where uc.user = :user")
+    List<SeaCreature> findSeaCreaturesByUser(@Param("user") User user);
+
 }
